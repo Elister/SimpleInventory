113a63e9a8883d30be64eb6d870735326bd7bbe6
Alexander Lyashenko <stilllnoash@gmail.com>
2017-11-15 14:53:19 +0300

    Added singleton access to ui area

---
+++ Assets/Scripts/Refrences.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Refrences : MonoBehaviour
+{
+
+	public static Refrences instance;
+
+	public GameView GameView;
+	
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+}
